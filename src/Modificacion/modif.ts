7d838f4d865c2cc7a9b8ba1b7f84eb92f2c54694
Alu0101329161 <alu0101329161@ull.edu.es>
2022-04-06 09:44:33 +0000

    Añadiendo modif

--- src/Modificacion/modif.ts
+++ src/Modificacion/modif.ts
@@ -1,129 +1,20 @@
 /**
- * Interfaz para los algortimos
+ * Interface for observable classes
  */
-export interface Strategy {
-  execute(dato: number[]): number[];
+export interface Observable {
+  subscribe(observer: Observer): void;
+  unsubscribe(observer: Observer): void;
+  notify(): void;
 }
 
 /**
- * @class clase que menja los algoritmos.
+ * Interface for observer classes
  */
-export class Solver {
-  /**
-   * Constructor
-   * @param data array de numeros 
-   * @param strategy algoritmo
-   */
-  constructor(private data: number[], private strategy: Strategy) { }
-
-  /**
-   * Cambia el algoritmo
-   * @param strategy algoritmo
-   */
-  setStrategy(strategy: Strategy) {
-    this.strategy = strategy;
-  }
-
-  /**
-   * Ejecuta el algoritmo
-   */
-  ordenar(): void {
-    this.data = this.strategy.execute(this.data);
-  }
-
-  /**
-   * Devuelve el array
-   * @returns array de numeros
-   */
-  getData(): number[] {
-    return this.data;
-  }
-
-  /**
-   * Cambia los numeros del array
-   * @param data nuevos numeros
-   */
-  setData(data: number[]): void {
-    this.data = data;
-  }
+export interface Observer {
+  update(observable: Observable): void;
 }
 
-/**
- * @class Bubblesort
- */
-export class BubbleSort implements Strategy {
-  /**
-   * Ejecuta el algortimo BubbleSort.
-   * @param dato array de numeros
-   */
-  execute(dato: number[]): number[] {
-    const l = dato.length;
-    for (let i = 0; i < l; i++) {
-      for (let j = 0; j < l - 1 - i; j++) {
-        if (dato[j] > dato[j + 1]) {
-          [dato[j], dato[j + 1]] = [dato[j + 1], dato[j]];
-        }
-      }
-    }
-    return dato;
-  }
-}
+export enum EventType {'Añadido Titulo', "Sin añadidos"};
 
-/**
-* @clas MergeSort
-*/
-export class MergeSort implements Strategy {
-  /**
-   * Ejecuta el algoritmo mergesort
-   * @param dato array de numeros
-   */
-  execute(dato: number[]): number[] {
-    return this.mergeSort(dato);
-  }
 
-  /**
-   * Algoritmo mergesort
-   * @param dato array de numeros
-   * @returns el array ordenado
-   */
-  mergeSort(dato: number[]): number[] {
-    if (dato.length < 2) {
-      return dato;
-    }
 
-    const medio = Math.floor(dato.length / 2);
-    const izquierda = dato.slice(0, medio);
-    const derecha = dato.slice(medio, dato.length);
-
-    return this.merge(this.mergeSort(izquierda), this.mergeSort(derecha));
-  }
-
-  /**
-   * ALgoritmo aux del Mersort
-   * @param izq array de numeros
-   * @param dere array de numeros
-   * @returns el array ordenado
-   */
-  merge(izq: number[], dere: number[]): number[] {
-    const aux: number[] = [];
-
-    while (izq.length && dere.length) {
-      if (izq[0] < dere[0]) {
-        aux.push(izq.shift() as number);
-      } else {
-        aux.push(dere.shift() as number);
-      }
-    }
-
-    while (izq.length) {
-      aux.push(izq.shift() as number);
-    }
-    while (dere.length) {
-      aux.push(dere.shift() as number);
-    }
-    return aux;
-  }
-}
-
-const a = new MergeSort;
-console.log(a.mergeSort([9, 8, 7, 6]));
